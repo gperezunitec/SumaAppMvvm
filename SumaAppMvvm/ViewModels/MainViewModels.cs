852f0a9b01e8d489e718d077366a42b3a679f23c
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-11 15:12:47 -0600

    ObservableObject

---
+++ SumaAppMvvm/ViewModels/MainViewModels.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SumaAppMvvm.ViewModels;
+
+public partial class MainViewModels: ObservableObject
+{
+
+    [ObservableProperty] 
+    private double a;
+    
+    [ObservableProperty]
+    private double b;
+    
+    [ObservableProperty]
+    private double c;
+    
+    
+    
+    private void Alerta(string Titulo, string Mensaje)
+    {
+        MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+    }
+
+}
---
+++ SumaAppMvvm/Views/MainView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SumaAppMvvm.ViewModels;
+
+namespace SumaAppMvvm.Views;
+
+
+
+public partial class MainView : ContentPage
+{
+    
+    
+    private MainViewModels viewModel;
+    
+    public MainView()
+    {
+        InitializeComponent();
+        viewModel = new MainViewModels();
+        BindingContext = viewModel;
+    }
+}
